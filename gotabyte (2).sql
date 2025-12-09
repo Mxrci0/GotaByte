fa23699bfb82e6520263cee06ccf33fe8483f754
Mxrci0 <juniorpires8969@gmail.com>
2025-12-08 21:27:16 -0300

    banco

--- gotabyte (2).sql
+++ gotabyte (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 07/12/2025 às 19:26
+-- Tempo de geração: 09/12/2025 às 00:26
 -- Versão do servidor: 9.1.0
 -- Versão do PHP: 8.3.14
 
@@ -24,23 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `feedback`
---
-
-DROP TABLE IF EXISTS `feedback`;
-CREATE TABLE IF NOT EXISTS `feedback` (
-  `fb_id` int NOT NULL AUTO_INCREMENT,
-  `fb_nome` varchar(150) NOT NULL,
-  `fb_email` varchar(150) NOT NULL,
-  `fb_rating` tinyint NOT NULL,
-  `fb_mensagem` text NOT NULL,
-  `fb_data` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`fb_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
 -- Estrutura para tabela `form_pessoafisica`
 --
 
@@ -53,25 +36,14 @@ CREATE TABLE IF NOT EXISTS `form_pessoafisica` (
   `tel` varchar(20) DEFAULT NULL,
   `senha` varchar(255) NOT NULL,
   PRIMARY KEY (`pf_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Despejando dados para a tabela `form_pessoafisica`
 --
 
 INSERT INTO `form_pessoafisica` (`pf_id`, `name`, `cpf`, `email`, `tel`, `senha`) VALUES
-(2, '231', '123456789', 'juniorpires8969@3.c1313om', '19995585892', '$2y$10$VnOp1L6LrCX3Dgw3a5YHBeW4ARR35R42jGxoAWq0dR99a8ULd9Z.G'),
-(3, '231', '123456789', 'juniorpires8969@3.c1313om', '19995585892', '$2y$10$yrCX4y4sL0Kkue6m5Rm0IeePLCETO585B79YI1rDQFNk4lxSVd8b2'),
-(4, 'MARCIO PIRES MAURE JUNIOR', 'q123456789', 'akdaskdk@hamr.com.br', '19995585892', '$2y$10$bKabmCqoUKJVNXtRmgKhUe5SqfYazFILV0LidixSRCCv9aErBlZdy'),
-(5, 'MARCIO PIRES MAURE JUNIOR', '025.032.938-75', 'juniorpires8969@gmail.com', '19995585892', '$2y$10$nJC5NpOdNkIXzY7J1sWq9.jpapPo7iQd2LU.joxKEKSvPLYeBSP4C'),
-(6, 'MARCIO PIRES MAURE JUNIOR1234', '123456789', 'juniorpires8969@gmail.com', '19995585892', '$2y$10$8rjVNZwHaEZxyk/cggzGYeRpseXUpTvz0oPGyC2ehlS9m9iA2YXhu'),
-(7, '', '', '', '', '$2y$10$tmgIGm/XV9.JatH9jn286uF5hVJw7w0wHI9LbzHlhuPtnfg4wKfUO'),
-(8, '', '', '', '', '$2y$10$DHwdEt146/WejM3bAZkpJO0RuToVer/QqHiHoHZCpN4YvzW31zQ3S'),
-(9, '', '', '', '', '$2y$10$llS5ei.s1uU5EZZnEOCxIe96/19aXuTp4mgTFpwHX044fYxOEEk6e'),
-(10, '', '', '', '', '$2y$10$PQklo54nm.thRlNN8qeCB.KL3iviyO0sOhJ2GblnEQ8Em6xolr2oO'),
-(11, 'MARCIO PIRES MAURE JUNIOR', '12345678', 'juniorpires8969@gmail.com', '19995585892', '$2y$10$MTV5TyGei2zHC6/wACGcf.p/XfU4PB4LqwQDrb7A4XAs7SAMQb3hC'),
-(12, 'MARCIO PIRES MAURE JUNIOR', '\'12323456ytfd', 'juniorpires8969@gmail.com', '19995585892', '$2y$10$uFy9Pn97MN9R/SugGMtHKeGKH0JWm/yRGdMGG7zBbJtx74pSZqBxu'),
-(13, 'MARCIO PIRES MAURE JUNIOR', 'q2w3e4r23456t2', 'juniorpires8969@gmail.com', '19995585892', '$2y$10$5C8ItHi4FV6DY.FZJNwfhu9ZiZG4BxMhDIvF5iIxW7rENMw17QBqm');
+(3, '231', '123456789', 'juniorpires8969@3.c1313om', '19995585892', '$2y$10$yrCX4y4sL0Kkue6m5Rm0IeePLCETO585B79YI1rDQFNk4lxSVd8b2');
 
 -- --------------------------------------------------------
 
@@ -92,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `routes` (
   `is_dynamic` tinyint(1) DEFAULT '0',
   `pattern` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `routes`
@@ -117,7 +89,9 @@ INSERT INTO `routes` (`id`, `nome_rota`, `slug`, `controller`, `action`, `status
 (16, 'form_Pre', 'form_Pre', 'AlunosController', 'form_Pre', 1, '2025-11-10 23:22:26', '2025-12-03 22:22:20', 0, '0'),
 (17, 'Formularios', 'usuarios', 'AlunosController', 'usuarios', 1, '2025-11-10 23:22:26', '2025-11-10 23:22:26', 0, NULL),
 (18, 'Login-inserir', 'login/inserir', 'AlunosController', 'inserir', 1, '2025-11-10 23:22:26', '2025-11-10 23:22:26', 0, NULL),
-(19, 'excluirpf', 'cadastros/excluir/pessoafisica/{id}', 'AlunosController', 'excluirpf', 1, '2025-12-07 18:37:37', '2025-12-07 18:45:38', 1, 'cadastros/excluir/pessoafisica/{id}');
+(19, 'excluirpf', 'cadastros/excluir/pessoafisica/{id}', 'AlunosController', 'excluirpf', 1, '2025-12-07 18:37:37', '2025-12-07 18:45:38', 1, 'cadastros/excluir/pessoafisica/{id}'),
+(20, 'Login-listar', 'login/listar', 'AlunosController', 'listarpf', 1, '2025-11-10 23:22:26', '2025-11-10 23:22:26', 0, NULL),
+(21, 'Login-editar', 'login/editar/pessoafisica/{id}', 'AlunosController', 'editarPf', 1, '2025-11-10 23:22:26', '2025-12-08 23:58:22', 1, 'login/editar/pessoafisica/{id}');
 
 -- --------------------------------------------------------
 
